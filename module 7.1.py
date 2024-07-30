38e7c9903f20e90c07370dfa0c1c7746e88b79cb
Magomeds05 <yusypovm@gmail.com>
2024-07-30 16:36:32 +0300

    Add files via upload

---
+++ module 7.1.py
@@ -0,0 +1,42 @@
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return self.name, self.weight, self.category
+
+
+class Shop:
+    def __init__(self, *file_name):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        from pprint import pprint
+        self.__file_name = 'products.txt'
+        file = open(self.__file_name, 'r')
+        pprint(file.read())
+        file.close()
+
+    def add(self, *products):
+        from pprint import pprint
+        self.__file_name = 'products.txt'
+        file = open(self.__file_name, 'a')
+        file.write(products)
+        file.close()
+
+
+
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
